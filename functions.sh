412983c490b043fab967d984047299cbc16d3fff
Jeremy Huntwork <jeremy@spotify.com>
2013-08-02 14:47:19 -0400

    Revert "Use mirror where possible"
    This reverts commit 3cc9e24e7bd64a2ab892cd27f2c2616368779318.

--- functions.sh
+++ functions.sh
@@ -1,14 +1,10 @@
 # Setup the dotfiles repo locally, or pull latest version from github.
 # Create symlinks in the $HOME directory to elements in the repo
-REPOGH='https://github.com/jhuntwork/dotfiles'
-REPOSP='https://ghe.spotify.net/jeremy/dotfiles'
-
 jpull() {
-    local REPO=$REPOGH
-    local SGIT=git
+    local REPO='https://github.com/jhuntwork/dotfiles.git'
+    local SGITURL='https://raw.github.com/jhuntwork/dotfiles/e374d0dbc1754b21a3d36b9df5742d351d7fe460/git-static-x86_64-linux-musl.tar.xz'
     local SGITPATH="${HOME}/.git-static"
-    local SGITURL="${REPO}/raw/e374d0dbc1754b21a3d36b9df5742d351d7fe460/git-static-x86_64-linux-musl.tar.xz"
-    [ "$(hostname | rev | cut -d. -f1,2)" = "ten.yfitops" ] && REPO=$REPOSP
+    SGIT=git
     if ! ${SGIT} --version >/dev/null 2>&1 ; then
         SGIT="${SGITPATH}/git --exec-path=${SGITPATH}/git-core"
         if ! ${SGIT} --version >/dev/null 2>&1 ; then
@@ -41,7 +37,8 @@ function jssh() {
 ssh -A -t "$@" \
     "[ -r /etc/motd ] && cat /etc/motd ;
     if ! type jpull >/dev/null 2>&1 ;
-      then eval \"\$(curl -sL $REPOGH/raw/master/functions.sh)\" && jpull;
+      then eval \"\$(curl -sL https://raw.github.com/jhuntwork/dotfiles/master/functions.sh)\" \
+      && jpull;
     fi;
     exec env -i SSH_AUTH_SOCK=\"\$SSH_AUTH_SOCK\" \
       SSH_CONNECTION=\"\$SSH_CONNECTION\" \
